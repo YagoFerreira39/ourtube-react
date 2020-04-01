ec6ebf78c8ab02bad3053851b884d649c08488b2
Yfer <yago@codeby.com.br>
2020-04-01 11:30:11 -0300

    React front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { Grid } from "@material-ui/core";
+
+import { SearchBar, VideoDetails, VideoList } from "./components";
+import youtube from "./api/Youtube";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const API_KEY = process.env.API_KEY;
+
+  const handlesubmit = async searchTerm => {
+    const res = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: API_KEY,
+        q: searchTerm
+      }
+    });
+    setVideos(res.data.items);
+    setSelectedVideo(res.data.items[0]);
+  };
+
+  const onVideoSelect = video => {
+    setSelectedVideo(video);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid justify="center" container spacing={10}>
+      <Grid item xs={12}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onFormSubmit={handlesubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetails video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
